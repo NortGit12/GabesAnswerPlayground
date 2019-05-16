61ac9d29542d53cbeeb8ad14b6791edfc93f2deb
Jeff Norton <jeff12ntn@gmail.com>
2019-05-16 09:21:29 -0600

    Initial project setup

---
+++ AsparagusIsDisgusting/AsparagusIsDisgusting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AsparagusIsDisgusting
+//
+//  Created by Jeff Norton on 5/16/19.
+//  Copyright © 2019 Oh Yeah, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
